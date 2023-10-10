051bbe7c6c61b253a13cda291dec6eec53e1af6b
Shivam raj <shivamraj82715@gmai.com>
2023-10-10 13:45:59 +0530

    firstt committ

--- Card.js
+++ Card.js
@@ -1,10 +1,10 @@
 import React from "react"
 export default function Card(){
 return (
-  <>
+  <div className="search">
   <input type="text" placeholder="Search food"></input>
     <button>Search</button>
    
-</>
+</div>
 )
 }
